bad500d0fe15d8c1a3f4d5259daf620366e7542e
Leyvi Silván <leyvicaz@gmail.com>
2017-08-27 08:56:13 -0500

    Lesson 20 :: Uso de eventos personalizados para solucionar el problema de elimnar una tarea sin la necesidad de pasar el listado de tareas a acada componente 'app-task'

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+Vue.component('app-icon',{
+    template: '<span :class="cssClasses" aria-hidden="true"></span>',
+    props: ['img'],
+    computed: {
+        cssClasses : function () {
+            return 'glyphicon glyphicon-'+this.img;
+        }
+    }
+})
+
+Vue.component('app-task',{
+    data : function () {
+        return {
+            editing : false,
+            draft : ''
+        };
+    },
+    template: '#task-template',
+    props : ['task', 'index'],
+    methods : {
+        toggleStatus : function () {
+            this.task.pending = !this.task.pending;
+        },
+        edit : function () {
+            //FIX ME
+            /*this.tasks.forEach(function (task) {
+                task.editing = false
+            })*/
+
+            this.draft = this.task.description;
+            this.editing = true;
+        },
+        update : function () {
+            this.task.description = this.draft;
+            this.editing = false;
+        },
+        discard : function () {
+            this.editing = false;
+        },
+        destroy : function () {
+            this.$emit('remove', this.index);
+        },
+    }
+})
 /**
  * Lección 13
  */
@@ -14,7 +58,7 @@ var vm = new Vue({
             {
                 description: 'Suscribirse a Styde.net',
                 pending: true,
-                editing: true
+                editing: false
             },
             {
                 description: 'Grabar lección de Vue',
@@ -32,6 +76,12 @@ var vm = new Vue({
             });
 
             this.new_task = '';
+        },
+        deleteTask : function (index) {
+            this.tasks.splice(index,1);
+        },
+        deleteCompleteTasks : function () {
+            this.tasks = this.tasks.filter(function (t) { return t.pending });
         }
     }
 });
